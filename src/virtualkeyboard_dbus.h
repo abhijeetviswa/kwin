216ecb6684279f49c43fe4ca151fe69938ecdc3b
Aleix Pol <aleixpol@kde.org>
2021-02-23 16:12:38 +0100

    inputmethod: Provide DBus API so the shell can integrate with it
    It makes it possible to show if the virtual keyboard is being shown and
    if it is to hide it.

--- src/virtualkeyboard_dbus.h
+++ src/virtualkeyboard_dbus.h
@@ -17,35 +17,30 @@ class VirtualKeyboardDBus : public QObject
 {
     Q_OBJECT
     Q_CLASSINFO("D-Bus Interface", "org.kde.kwin.VirtualKeyboard")
-    Q_PROPERTY(bool enabled READ isEnabled NOTIFY enabledChanged)
+    Q_PROPERTY(bool enabled READ isEnabled WRITE setEnabledRequest NOTIFY enabledChanged)
+    Q_PROPERTY(bool active READ isActive NOTIFY activeChanged)
 public:
     explicit VirtualKeyboardDBus(QObject *parent = nullptr);
     ~VirtualKeyboardDBus() override;
-    Q_INVOKABLE bool isEnabled() const {
-        return m_enabled;
-    }
-    void setEnabled(bool enabled) {
-        if (m_enabled == enabled) {
-            return;
-        }
-        m_enabled = enabled;
-        emit enabledChanged();
-    }
+    bool isEnabled() const;
+    void setEnabled(bool enabled);
+    void setEnabledRequest(bool enabled);
+
+    void setActive(bool active);
+    bool isActive() const;
 
 public Q_SLOTS:
-    void enable() {
-        emit activateRequested(true);
-    }
-    void disable() {
-        emit activateRequested(false);
-    }
+    void hide();
 
 Q_SIGNALS:
-    Q_SCRIPTABLE void enabledChanged();
-    void activateRequested(bool requested);
+    void enabledChanged();
+    void hideRequested();
+    void activeChanged();
+    void enableRequested(bool requested);
 
 private:
     bool m_enabled = false;
+    bool m_active = false;
 };
 
 }
